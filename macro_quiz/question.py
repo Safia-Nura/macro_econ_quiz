841523eefb8c1f45a711f06ef1f6f02aa7025183
Safia Mohamoud <safiamohamoud282@gmail.com>
2026-02-28 14:05:01 +0000

    docs : docstring added for file

--- macro_quiz/question.py
+++ macro_quiz/question.py
@@ -1,13 +1,16 @@
 class Question:
     # represents a single quick question
-    #attributes: test =  question text, options = list of four answers (strings), correct_index = index (0-3) of correct answers, topic = topic category the question belongs to and explanation: optional expliantion after the Q. is answered.
+    """attributes: test =  question text, options = list of four answers (strings), 
+    correct_index = index (0-3) of correct answers, 
+    topic = topic category the question belongs to and explanation: optional expliantion after the Q. is answered.
+    """
     def __init__(self, text:str, options:list,correct_index:int,
                  topic:str, explanation:str =""):
-        self.text =text
-        self.options =options
-        self.correct_index =correct_index
-        self.topic =topic
-        self.explanation =explanation
+        self.text = text
+        self.options = options
+        self.correct_index = correct_index
+        self.topic = topic
+        self.explanation = explanation
     
     def is_correct(self,answer_index: int) ->bool:
         #checks whether the answer is correct
